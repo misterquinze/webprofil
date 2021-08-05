2b1ca6345f492a491471fd74b466edb061ef203f
Achmad Alghifari <31951431+misterquinze@users.noreply.github.com>
2021-08-05 15:52:35 +0700

    cache helper

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+    KEY: '',
+    BASE_URL: '',
+    BASE_IMAGE_URL: '',
+    DEFAULT_LANGUAGE: 'en-us',
+    CACHE_NAME: 'N-page',
+    DATABASE_NAME: 'N-database',
+    DATABASE_VERSION: 1,
+    OBJECT_STORE_NAME: 'natsir',
+    API_KEY: 12345,
+  };
+  export default CONFIG;
+  
